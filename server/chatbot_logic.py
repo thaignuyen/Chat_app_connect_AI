7cb1bf6f49190f534940968aaf8e712a8813e553
thaignuyen <thainguyen2006.06@gmail.com>
2026-03-12 14:58:17 +0700

    Demo chatapp

--- server/chatbot_logic.py
+++ server/chatbot_logic.py
@@ -1,9 +1,16 @@
 import google.generativeai as genai
+import os 
 
-genai.configure(api_key="AIzaSyCk1lzJ36Xg6x3o4JR4JhdS3ATO6YbNr0E")
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
-def get_ai_response(prompt):
-    res = model.generate_content(prompt)
-    return res.text
+def get_ai_response(prompt : str) -> str:
+    try : 
+        reponse = model.generate_content(prompt)
+        if reponse.text:
+            return reponse.text
+        return "AI không thể trả nội dung vì đã hết quota "
+            
+    except Exception as e:
+        return f"Đã có lỗi xảy ra khi gọi API: {repr(e)}"
